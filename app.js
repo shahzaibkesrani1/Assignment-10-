299de97073aa0c1a466be00d3735728b76129317
ShahzaibKesrani <111377043+shahzaibkesrani1@users.noreply.github.com>
2023-02-05 07:52:36 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var mainIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+
+mainIcon.onclick = function(){
+    sidebar.classList.toggle("newsidebar")
+}
